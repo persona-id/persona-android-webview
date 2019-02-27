742cf5d64c4256f5be80dfef74fb247f022e1ab2
Charles Yeh <charles@withpersona.com>
2019-02-27 12:09:14 -0800

    Add file chooser for photo selection

--- app/src/main/java/com/google/chrome/android/webrtcsample/MainActivity.java
+++ app/src/main/java/com/google/chrome/android/webrtcsample/MainActivity.java
@@ -7,63 +7,67 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
-import android.view.View;
 import android.webkit.CookieManager;
+import android.webkit.ValueCallback;
 import android.webkit.WebChromeClient;
-import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.PermissionRequest;
 import android.webkit.WebViewClient;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
 
 
 public class MainActivity extends Activity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
+    public static final int INPUT_FILE_REQUEST_CODE = 1;
+
+    private WebView webView;
+    private ValueCallback<Uri[]> filePathCallback;
+    private String cameraPhotoPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final WebView personaWebView = (WebView) findViewById(R.id.fragment_main_webview);
+        webView = (WebView) findViewById(R.id.fragment_main_webview);
 
-        setUpWebViewDefaults(personaWebView);
+        setUpWebViewDefaults(webView);
 
         // Initialize Persona
         HashMap<String, String> personaOptions = new HashMap<>();
-        personaOptions.put("is-webview", "true");
 
-        personaOptions.put("blueprint-id", "blu_M1ivtd7uaiZQBESJvR4tN8Fu");
+        personaOptions.put("blueprint-id", "blu_MvKhz42SXzr8akDa7jZJ1Esv");
         personaOptions.put("redirect-uri", "https://personademo.com");
-        personaOptions.put("baseUrl", "https://withpersona.com:3000/verify");
+        personaOptions.put("baseUrl", "https://withpersona.com/verify");
         final Uri personaUrl = generatePersonaUrl(personaOptions);
+        Log.d("Loading URL: ", personaUrl.toString());
 
-        personaWebView.loadUrl(personaUrl.toString());
+        webView.loadUrl(personaUrl.toString());
 
-        personaWebView.setWebViewClient(new WebViewClient() {
+        webView.setWebViewClient(new WebViewClient() {
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
                 Uri parsedUri = Uri.parse(url);
-                if (parsedUri.getScheme().equals("persona")) {
-                    String action = parsedUri.getHost();
-                    HashMap<String, String> linkData = parsePersonaUriData(parsedUri);
+                if (parsedUri.getAuthority().equals("personademo.com")) {
+                    Log.d("Success callback URL: ", parsedUri.toString());
 
-                    if (action.equals("start")) {
-                        // User start an inquiry
-                        Log.d("Inquiry Id: ", linkData.get("inquiryId"));
-                        Log.d("Subject: ", linkData.get("subject"));
-                    } else if (action.equals("success")) {
-                        // User succeeded verification
+                    HashMap<String, String> uriData = parsePersonaUriData(parsedUri);
+                    Log.d("Inquiry Id: ", uriData.get("inquiry-id"));
+                    Log.d("Subject: ", uriData.get("subject"));
 
-                        // Reload Persona in the Webview
-                        // You will likely want to transition the view at this point.
-                        personaWebView.loadUrl(personaUrl.toString());
-                    }
-                    // Override URL loading
+                    // Reload Persona in the Webview
+                    // You will likely want to transition the view at this point.
+                    webView.loadUrl(personaUrl.toString());
                     return true;
                 } else if (parsedUri.getScheme().equals("https") ||
                         parsedUri.getScheme().equals("http")) {
@@ -78,8 +82,7 @@ public class MainActivity extends Activity {
             }
         });
 
-        personaWebView.setWebChromeClient(new WebChromeClient() {
-
+        webView.setWebChromeClient(new WebChromeClient() {
             @Override
             public void onPermissionRequest(final PermissionRequest request) {
                 Log.d(TAG, "onPermissionRequest");
@@ -87,7 +90,7 @@ public class MainActivity extends Activity {
                     @TargetApi(Build.VERSION_CODES.LOLLIPOP)
                     @Override
                     public void run() {
-                        if(request.getOrigin().toString().equals("https://apprtc-m.appspot.com/")) {
+                        if(request.getOrigin().toString().equals("https://withpersona.com/")) {
                             request.grant(request.getResources());
                         } else {
                             request.deny();
@@ -96,7 +99,109 @@ public class MainActivity extends Activity {
                 });
             }
 
+            @Override
+            public boolean onShowFileChooser(
+                    WebView webView, ValueCallback<Uri[]> newFilePathCallback,
+                    FileChooserParams fileChooserParams) {
+
+                if (filePathCallback != null) {
+                    filePathCallback.onReceiveValue(null);
+                }
+                filePathCallback = newFilePathCallback;
+
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                    // Create the File where the photo should go
+                    File photoFile = null;
+                    try {
+                        photoFile = createImageFile();
+                        takePictureIntent.putExtra("PhotoPath", cameraPhotoPath);
+                    } catch (IOException ex) {
+                        // Error occurred while creating the File
+                        Log.e(TAG, "Unable to create Image File", ex);
+                    }
+
+                    // Continue only if the File was successfully created
+                    if (photoFile != null) {
+                        cameraPhotoPath = "file:" + photoFile.getAbsolutePath();
+                        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,
+                                Uri.fromFile(photoFile));
+                    } else {
+                        takePictureIntent = null;
+                    }
+                }
+
+                Intent contentSelectionIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                contentSelectionIntent.addCategory(Intent.CATEGORY_OPENABLE);
+                contentSelectionIntent.setType("image/*");
+
+                Intent[] intentArray;
+                if (takePictureIntent != null) {
+                    intentArray = new Intent[]{takePictureIntent};
+                } else {
+                    intentArray = new Intent[0];
+                }
+
+                Intent chooserIntent = new Intent(Intent.ACTION_CHOOSER);
+                chooserIntent.putExtra(Intent.EXTRA_INTENT, contentSelectionIntent);
+                chooserIntent.putExtra(Intent.EXTRA_TITLE, "Image Chooser");
+                chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, intentArray);
+
+                startActivityForResult(chooserIntent, INPUT_FILE_REQUEST_CODE);
+
+                return true;
+            }
         });
+    }
+
+    @Override
+    public void onActivityResult (int requestCode, int resultCode, Intent data) {
+        if(requestCode != INPUT_FILE_REQUEST_CODE || filePathCallback == null) {
+            super.onActivityResult(requestCode, resultCode, data);
+            return;
+        }
+
+        Uri[] results = null;
+
+        // Check that the response is a good one
+        if(resultCode == Activity.RESULT_OK) {
+            if(data == null) {
+                // If there is not data, then we may have taken a photo
+                if(cameraPhotoPath != null) {
+                    results = new Uri[]{Uri.parse(cameraPhotoPath)};
+                }
+            } else {
+                String dataString = data.getDataString();
+                if (dataString != null) {
+                    results = new Uri[]{Uri.parse(dataString)};
+                }
+            }
+        }
+
+        filePathCallback.onReceiveValue(results);
+        filePathCallback = null;
+        return;
+    }
+
+    /**
+     * More info this method can be found at
+     * http://developer.android.com/training/camera/photobasics.html
+     *
+     * @return
+     * @throws IOException
+     */
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES);
+        File imageFile = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+        return imageFile;
     }
 
     private void setUpWebViewDefaults(WebView webView) {
@@ -109,8 +214,8 @@ public class MainActivity extends Activity {
         settings.setUseWideViewPort(true);
         settings.setLoadWithOverviewMode(true);
 
-        // Enable pinch to zoom without the zoom buttons
-        settings.setBuiltInZoomControls(true);
+        // Disable pinch to zoom without the zoom buttons
+        settings.setBuiltInZoomControls(false);
 
         // Allow use of Local Storage
         settings.setDomStorageEnabled(true);
@@ -134,8 +239,7 @@ public class MainActivity extends Activity {
     public Uri generatePersonaUrl(HashMap<String,String> personaOptions) {
         Uri.Builder builder = Uri.parse(personaOptions.get("baseUrl"))
                 .buildUpon()
-                .appendQueryParameter("isWebview", "true")
-                .appendQueryParameter("isMobile", "true");
+                .appendQueryParameter("is-webview", "true");
         for (String key : personaOptions.keySet()) {
             if (!key.equals("baseUrl")) {
                 builder.appendQueryParameter(key, personaOptions.get(key));
