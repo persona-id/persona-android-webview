45d4006049b2625fb9902b57cf36191356e0e566
Salvatore Testa <sal@withpersona.com>
2019-11-13 14:45:11 -0800

    Re-indent the XML, Groovy, and Java files
    Otherwise, auto-formatters will change these every time.

--- app/src/main/java/com/withpersona/webview/MainActivity.java
+++ app/src/main/java/com/withpersona/webview/MainActivity.java
@@ -19,214 +19,212 @@ import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-
 public class MainActivity extends Activity {
 
-    private static final String TAG = MainActivity.class.getSimpleName();
-    public static final int INPUT_FILE_REQUEST_CODE = 1;
+  public static final int INPUT_FILE_REQUEST_CODE = 1;
+  private static final String TAG = MainActivity.class.getSimpleName();
+  private WebView webView;
+  private ValueCallback<Uri[]> filePathCallback;
+  private String cameraPhotoPath;
 
-    private WebView webView;
-    private ValueCallback<Uri[]> filePathCallback;
-    private String cameraPhotoPath;
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    webView = findViewById(R.id.fragment_main_webview);
 
-        webView = (WebView) findViewById(R.id.fragment_main_webview);
+    setUpWebViewDefaults(webView);
 
-        setUpWebViewDefaults(webView);
+    final Uri personaUrl = new Uri.Builder()
+        .scheme("https")
+        .encodedAuthority("withpersona.com")
+        .path("verify")
+        .appendQueryParameter("is-webview", "true")
+        .appendQueryParameter("template-id", "tmpl_JAZjHuAT738Q63BdgCuEJQre")
+        .appendQueryParameter("environment", "sandbox")
+        .build();
 
-        final Uri personaUrl = new Uri.Builder()
-            .scheme("https")
-            .encodedAuthority("withpersona.com")
-            .path("verify")
-            .appendQueryParameter("is-webview", "true")
-            .appendQueryParameter("template-id", "tmpl_JAZjHuAT738Q63BdgCuEJQre")
-            .appendQueryParameter("environment", "sandbox")
-            .build();
+    webView.loadUrl(personaUrl.toString());
 
-        webView.loadUrl(personaUrl.toString());
+    webView.setWebViewClient(new WebViewClient() {
 
-        webView.setWebViewClient(new WebViewClient() {
+      @Override
+      public boolean shouldOverrideUrlLoading(WebView view, String url) {
+        Uri parsedUri = Uri.parse(url);
+        if (parsedUri.getAuthority().equals("personacallback")) {
 
-            @Override
-            public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                Uri parsedUri = Uri.parse(url);
-                if (parsedUri.getAuthority().equals("personacallback")) {
+          // User succeeded verification
+          String inquiryID = parsedUri.getQueryParameter("inquiry-id");
+          inquiryID = inquiryID == null ? "none" : inquiryID;
+          Toast.makeText(webView.getContext(), "The inquiry ID is " + inquiryID,
+              Toast.LENGTH_SHORT)
+              .show();
 
-                    // User succeeded verification
-                    String inquiryID = parsedUri.getQueryParameter("inquiry-id");
-                    inquiryID = inquiryID == null ? "none" : inquiryID;
-                    Toast.makeText(webView.getContext(), "The inquiry ID is " + inquiryID,
-                        Toast.LENGTH_SHORT)
-                        .show();
+          // Reload Persona in the Webview
+          // You will likely want to transition the view at this point.
+          webView.loadUrl(personaUrl.toString());
 
-                    // Reload Persona in the Webview
-                    // You will likely want to transition the view at this point.
-                    webView.loadUrl(personaUrl.toString());
-
-                    // Override URL loading
-                    return true;
-                } else if (parsedUri.getScheme().equals("https") ||
-                        parsedUri.getScheme().equals("http")) {
-                    // Open in browser - this is most likely external help links
-                    view.getContext().startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
-                    // Override URL loading
-                    return true;
-                } else {
-                    // Unknown case - do not override URL loading
-                    return false;
-                }
-            }
-        });
-
-        webView.setWebChromeClient(new WebChromeClient() {
-
-            @Override
-            public void onPermissionRequest(final PermissionRequest request) {
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        if(request.getOrigin().toString().equals("https://withpersona.com/")) {
-                            request.grant(request.getResources());
-                        } else {
-                            request.deny();
-                        }
-                    }
-                });
-            }
-
-            @Override
-            public boolean onShowFileChooser(
-                    WebView webView, ValueCallback<Uri[]> newFilePathCallback,
-                    FileChooserParams fileChooserParams) {
-
-                if (filePathCallback != null) {
-                    filePathCallback.onReceiveValue(null);
-                }
-                filePathCallback = newFilePathCallback;
-
-                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-                    // Create the File where the photo should go
-                    File photoFile = null;
-                    try {
-                        photoFile = createImageFile();
-                        takePictureIntent.putExtra("PhotoPath", cameraPhotoPath);
-                    } catch (IOException ex) {
-                        // Error occurred while creating the File
-                    }
-
-                    // Continue only if the File was successfully created
-                    if (photoFile != null) {
-                        cameraPhotoPath = "file:" + photoFile.getAbsolutePath();
-                        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,
-                                Uri.fromFile(photoFile));
-                    } else {
-                        takePictureIntent = null;
-                    }
-                }
-
-                Intent contentSelectionIntent = new Intent(Intent.ACTION_GET_CONTENT);
-                contentSelectionIntent.addCategory(Intent.CATEGORY_OPENABLE);
-                contentSelectionIntent.setType("image/*");
-
-                Intent[] intentArray;
-                if (takePictureIntent != null) {
-                    intentArray = new Intent[]{takePictureIntent};
-                } else {
-                    intentArray = new Intent[0];
-                }
-
-                Intent chooserIntent = new Intent(Intent.ACTION_CHOOSER);
-                chooserIntent.putExtra(Intent.EXTRA_INTENT, contentSelectionIntent);
-                chooserIntent.putExtra(Intent.EXTRA_TITLE, "Image Chooser");
-                chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, intentArray);
-
-                startActivityForResult(chooserIntent, INPUT_FILE_REQUEST_CODE);
-
-                return true;
-            }
-        });
-    }
-
-    @Override
-    public void onActivityResult (int requestCode, int resultCode, Intent data) {
-        if(requestCode != INPUT_FILE_REQUEST_CODE || filePathCallback == null) {
-            super.onActivityResult(requestCode, resultCode, data);
-            return;
+          // Override URL loading
+          return true;
+        } else if (parsedUri.getScheme().equals("https") ||
+            parsedUri.getScheme().equals("http")) {
+          // Open in browser - this is most likely external help links
+          view.getContext().startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
+          // Override URL loading
+          return true;
+        } else {
+          // Unknown case - do not override URL loading
+          return false;
         }
+      }
+    });
 
-        Uri[] results = null;
+    webView.setWebChromeClient(new WebChromeClient() {
 
-        // Check that the response is a good one
-        if(resultCode == Activity.RESULT_OK) {
-            if(data == null) {
-                // If there is not data, then we may have taken a photo
-                if(cameraPhotoPath != null) {
-                    results = new Uri[]{Uri.parse(cameraPhotoPath)};
-                }
+      @Override
+      public void onPermissionRequest(final PermissionRequest request) {
+        runOnUiThread(new Runnable() {
+          @Override
+          public void run() {
+            if (request.getOrigin().toString().equals("https://withpersona.com/")) {
+              request.grant(request.getResources());
             } else {
-                String dataString = data.getDataString();
-                if (dataString != null) {
-                    results = new Uri[]{Uri.parse(dataString)};
-                }
+              request.deny();
             }
+          }
+        });
+      }
+
+      @Override
+      public boolean onShowFileChooser(
+          WebView webView, ValueCallback<Uri[]> newFilePathCallback,
+          FileChooserParams fileChooserParams) {
+
+        if (filePathCallback != null) {
+          filePathCallback.onReceiveValue(null);
+        }
+        filePathCallback = newFilePathCallback;
+
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+          // Create the File where the photo should go
+          File photoFile = null;
+          try {
+            photoFile = createImageFile();
+            takePictureIntent.putExtra("PhotoPath", cameraPhotoPath);
+          } catch (IOException ex) {
+            // Error occurred while creating the File
+          }
+
+          // Continue only if the File was successfully created
+          if (photoFile != null) {
+            cameraPhotoPath = "file:" + photoFile.getAbsolutePath();
+            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,
+                Uri.fromFile(photoFile));
+          } else {
+            takePictureIntent = null;
+          }
         }
 
-        filePathCallback.onReceiveValue(results);
-        filePathCallback = null;
+        Intent contentSelectionIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        contentSelectionIntent.addCategory(Intent.CATEGORY_OPENABLE);
+        contentSelectionIntent.setType("image/*");
+
+        Intent[] intentArray;
+        if (takePictureIntent != null) {
+          intentArray = new Intent[] { takePictureIntent };
+        } else {
+          intentArray = new Intent[0];
+        }
+
+        Intent chooserIntent = new Intent(Intent.ACTION_CHOOSER);
+        chooserIntent.putExtra(Intent.EXTRA_INTENT, contentSelectionIntent);
+        chooserIntent.putExtra(Intent.EXTRA_TITLE, "Image Chooser");
+        chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, intentArray);
+
+        startActivityForResult(chooserIntent, INPUT_FILE_REQUEST_CODE);
+
+        return true;
+      }
+    });
+  }
+
+  @Override
+  public void onActivityResult(int requestCode, int resultCode, Intent data) {
+    if (requestCode != INPUT_FILE_REQUEST_CODE || filePathCallback == null) {
+      super.onActivityResult(requestCode, resultCode, data);
+      return;
     }
 
-    /**
-     * More info this method can be found at
-     * http://developer.android.com/training/camera/photobasics.html
-     *
-     * @throws IOException
-     */
-    private File createImageFile() throws IOException {
-        // Create an image file name
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String imageFileName = "JPEG_" + timeStamp + "_";
-        File storageDir = Environment.getExternalStoragePublicDirectory(
-                Environment.DIRECTORY_PICTURES);
-        File imageFile = File.createTempFile(
-                imageFileName,  /* prefix */
-                ".jpg",         /* suffix */
-                storageDir      /* directory */
-        );
-        return imageFile;
+    Uri[] results = null;
+
+    // Check that the response is a good one
+    if (resultCode == Activity.RESULT_OK) {
+      if (data == null) {
+        // If there is not data, then we may have taken a photo
+        if (cameraPhotoPath != null) {
+          results = new Uri[] { Uri.parse(cameraPhotoPath) };
+        }
+      } else {
+        String dataString = data.getDataString();
+        if (dataString != null) {
+          results = new Uri[] { Uri.parse(dataString) };
+        }
+      }
     }
 
-    private void setUpWebViewDefaults(WebView webView) {
-        WebSettings settings = webView.getSettings();
+    filePathCallback.onReceiveValue(results);
+    filePathCallback = null;
+  }
 
-        // Enable Javascript
-        settings.setJavaScriptEnabled(true);
+  /**
+   * More info this method can be found at
+   * http://developer.android.com/training/camera/photobasics.html
+   *
+   * @throws IOException
+   */
+  private File createImageFile() throws IOException {
+    // Create an image file name
+    String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+    String imageFileName = "JPEG_" + timeStamp + "_";
+    File storageDir = Environment.getExternalStoragePublicDirectory(
+        Environment.DIRECTORY_PICTURES);
+    File imageFile = File.createTempFile(
+        imageFileName,  /* prefix */
+        ".jpg",         /* suffix */
+        storageDir      /* directory */
+    );
+    return imageFile;
+  }
 
-        // This is necessary to re-enable autoplay on camera videos
-        settings.setMediaPlaybackRequiresUserGesture(false);
+  private void setUpWebViewDefaults(WebView webView) {
+    WebSettings settings = webView.getSettings();
 
-        // Use WideViewport and Zoom out if there is no viewport defined
-        settings.setUseWideViewPort(true);
-        settings.setLoadWithOverviewMode(true);
+    // Enable Javascript
+    settings.setJavaScriptEnabled(true);
 
-        // Disable pinch to zoom without the zoom buttons
-        settings.setBuiltInZoomControls(false);
+    // This is necessary to re-enable autoplay on camera videos
+    settings.setMediaPlaybackRequiresUserGesture(false);
 
-        // Allow use of Local Storage
-        settings.setDomStorageEnabled(true);
+    // Use WideViewport and Zoom out if there is no viewport defined
+    settings.setUseWideViewPort(true);
+    settings.setLoadWithOverviewMode(true);
 
-        // Hide the zoom controls for HONEYCOMB+
-        settings.setDisplayZoomControls(false);
+    // Disable pinch to zoom without the zoom buttons
+    settings.setBuiltInZoomControls(false);
 
-        // Enable remote debugging via chrome://inspect
-        WebView.setWebContentsDebuggingEnabled(true);
+    // Allow use of Local Storage
+    settings.setDomStorageEnabled(true);
 
-        // AppRTC requires third party cookies to work
-        CookieManager cookieManager = CookieManager.getInstance();
-        cookieManager.setAcceptThirdPartyCookies(webView, true);
-    }
+    // Hide the zoom controls for HONEYCOMB+
+    settings.setDisplayZoomControls(false);
+
+    // Enable remote debugging via chrome://inspect
+    WebView.setWebContentsDebuggingEnabled(true);
+
+    // AppRTC requires third party cookies to work
+    CookieManager cookieManager = CookieManager.getInstance();
+    cookieManager.setAcceptThirdPartyCookies(webView, true);
+  }
 }
